aa06dfe66166fbcf837cf3504043e8c2275b4fb9
thanatos9516 <chepe352013@gmail.com>
2019-08-03 22:21:24 -0600

    Update db_sistema.sql

--- bd/25-07-2019/db_sistema.sql
+++ bd/25-07-2019/db_sistema.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 26-07-2019 a las 07:48:30
+-- Tiempo de generación: 04-08-2019 a las 06:20:36
 -- Versión del servidor: 10.3.16-MariaDB
 -- Versión de PHP: 7.3.6
 
